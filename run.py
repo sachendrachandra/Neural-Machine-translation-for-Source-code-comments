0923f5ed631e58f853fbffd9fb4750ef46e16071
sachendrachandra <58558221+sachendrachandra@users.noreply.github.com>
2021-01-20 23:41:43 +0530

    Update run.py

--- run.py
+++ run.py
@@ -256,14 +256,14 @@ def data_process(vocab,file,maxl):
   
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-src_train = torch.tensor(data_process(sys.argv[1],sys.argv[3],100)).to(device=device)
-trg_train = torch.tensor(data_process(sys.argv[2],sys.argv[4],20)).to(device=device)
+src_train = torch.tensor(data_process(sys.argv[2],sys.argv[4],100)).to(device=device)
+trg_train = torch.tensor(data_process(sys.argv[3],sys.argv[5],20)).to(device=device)
 
-src_test = torch.tensor(data_process(sys.argv[1],sys.argv[5],100)).to(device=device)
-trg_test = torch.tensor(data_process(sys.argv[2],sys.argv[6],20)).to(device=device)
+src_test = torch.tensor(data_process(sys.argv[2],sys.argv[6],100)).to(device=device)
+trg_test = torch.tensor(data_process(sys.argv[3],sys.argv[7],20)).to(device=device)
 
-src_valid = torch.tensor(data_process(sys.argv[1],sys.argv[7],100)).to(device=device)
-trg_valid = torch.tensor(data_process(sys.argv[2],sys.argv[8],20)).to(device=device)
+src_valid = torch.tensor(data_process(sys.argv[2],sys.argv[8],100)).to(device=device)
+trg_valid = torch.tensor(data_process(sys.argv[3],sys.argv[9],20)).to(device=device)
 
 
 train=[]
@@ -399,4 +399,4 @@ for epoch in range(N_EPOCHS):
 test_loss = evaluate(model, test_iter, criterion)
 print("test loss: ",test_loss)
 
-torch.save(model,sys.argv[9])
+torch.save(model,sys.argv[10])
